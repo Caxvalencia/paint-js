fa2e4d6400a7f347146ca52c8e8181929b51ba88
Caxvalencia <caxvalencia@gmail.com>
2018-05-01 23:25:08 -0500

    Add ts-loader dependency and add configuration to webpack file

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,15 @@ const config = {
     mode: process.env.NODE_ENV || 'production',
     entry: './src/index.ts',
     devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
     resolve: {
         extensions: ['.tsx', '.ts', '.js']
     },
